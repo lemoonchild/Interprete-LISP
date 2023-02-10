d359a036addb6db8ea2eb94b16b933ed890e1a77
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2023-02-10 17:39:18 -0600

    prueba incial con lisp

---
+++ prueba.lisp
@@ -0,0 +1,14 @@
+(
+    defun hi()
+        (print "hola mundo")
+)
+
+(
+    defun fahrenheit-to-celsius (fahrenheit)
+    (print (/ (- fahrenheit 32) 1.8))
+  )
+
+
+(hi)
+(fahrenheit-to-celsius 212)
+
