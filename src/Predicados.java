6cd3c8f4686cde8e8d4c42d973fa20fe795c7f99
Fabiola Contreras <fcontrerascol@gmail.com>
2023-03-10 11:27:47 -0600

    Adición procesos de predicados

--- src/Predicados.java
+++ src/Predicados.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+
 /**
  * Predicados
  */
-public class Predicados {
+public class Predicados<T> {
+
+    public boolean Atom(String elemento) {
+        if (elemento.contains("(") & elemento.contains(")")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean Equal(String e1, String e2) {
+        if (e1.equalsIgnoreCase(e2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean LessThan(Double e1, Double e2) {
+        if (e1 < e2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean MoreThan(Double e1, Double e2) {
+        if (e1 > e2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
--- src/SintaxScann.java
+++ src/SintaxScann.java
@@ -28,6 +28,16 @@ public class SintaxScann {
         } else if (Parts[0].equalsIgnoreCase("(quote") || Parts[1].equalsIgnoreCase("quote")
                 || Parts[0].indexOf("'") == 0) {
             return 3; // Tomar datos literales
+        } else if (Parts[0].equalsIgnoreCase("(atom") || Parts[1].equalsIgnoreCase("atom")) {
+            return 4; // ¿será una lista? - PREDICADO
+        } else if (Parts[0].equalsIgnoreCase("(equal") || Parts[1].equalsIgnoreCase("equal")) {
+            return 5; // ¿elementos iguales? - PREDICADO
+        } else if (Parts[0].equalsIgnoreCase("(<") || Parts[1].equalsIgnoreCase("<")) {
+            return 6; // Menor que - PREDICADO
+        } else if (Parts[0].equalsIgnoreCase("(>") || Parts[1].equalsIgnoreCase(">")) {
+            return 7; // Mayor que - PREDICADO
+        } else if (Parts[0].equalsIgnoreCase("(list") || Parts[1].equalsIgnoreCase("list")) {
+            return 8; // Crear una list
         } else {
             return 0; // Invalid action
         }
@@ -63,14 +73,15 @@ public class SintaxScann {
 
         return sf;
     }
-	public boolean evalateOperation(String regex, String expression) {
-        
-        String newExpression = expression.replace("(", "");
-        newExpression = expression.replace(")", ""); 
 
-		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-	    Matcher matcher = pattern.matcher(newExpression);
-        
-	    return matcher.find();
-	}
+    public boolean evalateOperation(String regex, String expression) {
+
+        String newExpression = expression.replace("(", "");
+        newExpression = expression.replace(")", "");
+
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(newExpression);
+
+        return matcher.find();
+    }
 }
