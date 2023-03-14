4b4d57201808a4adb12d45c8d236907bf6b42ee2
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-03-14 09:27:35 -0600

    Cambio de SetQ a clase "Variable"

--- src/Variables.java
+++ src/Variables.java
@@ -1,6 +1,15 @@
 /**
  * Variables
  */
-public class Variables {
+public class Variables<T> {
+    T _variable;
+
+    public Variables(T variable) {
+        this._variable = variable;
+    }
+
+    public <V> void SetQ (V aa){
+        this._variable = (T) aa;
+    }
 
 }
