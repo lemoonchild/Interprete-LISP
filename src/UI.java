6542c5be0daa97493dedcd2ad832dfc6a81a26e9
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-03-17 06:56:12 -0600

    Actualización en UI

--- src/UI.java
+++ src/UI.java
@@ -9,10 +9,13 @@ public class UI {
     static Scanner sc = new Scanner(System.in); 
     static Operations arithOp = new Operations();
     static readFile read = new readFile();
+    static Interpreter interpreter = new Interpreter<>();
 
 
     public static void main(String[] args) {
-        Leer_un_codigo();
+        ArrayList<ArrayList<String>> Array_sucio = read._readfile("C:\\Users\\villa\\Desktop\\Clases_S3\\Proyecto-LISP\\src\\Prueba.txt"); 
+        interpreter.Interp(Array_sucio);
+        System.out.println("Fin del Programa");
     }
 
     public static void menuPrincipalLISP() {
