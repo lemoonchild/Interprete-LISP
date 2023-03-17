6a8c3ebc1d21a52bf7b95a510a2e3da85cf259aa
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-03-16 21:48:59 -0600

    Agregar prueba de SetQ

---
+++ src/SetQ.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SetQ {
+    static readFile read = new readFile();
+    static Variables variables = new Variables<>();
+    static ArrayList<String> araaaa = new ArrayList<String>(); 
+
+    public static <T> void setQ_parame(ArrayList<String> exprAssignValue, String delimiter, HashMap<String, T> variables) {
+        araaaa.add("(setq x 10)");
+        ArrayList<String> tokens = read.split(exprAssignValue, delimiter);
+        for (String string : tokens) {
+            string.replace("\\(|\\)", "");
+    
+            
+        }
+        if (tokens.size() < 3 || !tokens.get(0).contains("setq")) {
+            
+            throw new IllegalArgumentException("Este no es un SetQ");
+        }
+    
+        String variableName = tokens.get(1);
+        T varValue = (T) tokens.get(2);
+
+        variables.put(variableName, varValue);
+    }
+
+    public static void main(String[] args) {
+        setQ_parame(araaaa, " ", variables.getVariables());
+        System.out.println(variables.getVariables());
+    }
+}
