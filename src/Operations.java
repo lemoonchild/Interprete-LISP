6fbf56d20cea916c80ef644638c2adc266ab0301
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-03-19 21:05:38 -0600

    Modificaciones en Operations

--- src/Operations.java
+++ src/Operations.java
@@ -168,17 +168,18 @@ public class Operations<T extends Number> {
      * @return Resultado que forma los tokens dentro de la lista
      */
     private void evaluatePendingTokens(List<Object> tokens) {
-        while (tokens.size() > 1 && tokens.get(tokens.size() - 2) instanceof Double) { // verifica si el valor de la
+     // verifica si el valor de la
                                                                                        // lista es mayor a 1 y que el
                                                                                        // penultimo valor de la lista es
                                                                                        // de tipo double
             double b = (double) tokens.remove(tokens.size() - 1);
-            double a = (double) tokens.remove(tokens.size() - 1);
+            Object cc = (Object) tokens.remove(tokens.size() - 1);
+            double a = Double.valueOf(cc.toString());
             String operator = (String) tokens.remove(tokens.size() - 1);
             double result = evaluateOperator(a, b, operator);
             stack.push(result);
             tokens.add(result);
-        }
+        
     }
 
     /**
